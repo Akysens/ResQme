b90398255998008947ec1c7c0c20d746b0cfe0ff
Enes A <enesaksay06@gmail.com>
2024-02-17 07:05:07 +0100

    Write expo functions to use Nearby API

--- modules/helphub-nearby/android/src/main/java/expo/modules/helphubnearby/HelphubNearbyModule.kt
+++ modules/helphub-nearby/android/src/main/java/expo/modules/helphubnearby/HelphubNearbyModule.kt
@@ -4,6 +4,7 @@ import expo.modules.kotlin.modules.Module
 import expo.modules.kotlin.modules.ModuleDefinition
 
 import android.content.Context;
+import android.util.Log
 import com.google.android.gms.nearby.Nearby;
 import com.google.android.gms.nearby.connection.AdvertisingOptions;
 import com.google.android.gms.nearby.connection.BandwidthInfo;
@@ -11,7 +12,6 @@ import com.google.android.gms.nearby.connection.ConnectionInfo;
 import com.google.android.gms.nearby.connection.ConnectionLifecycleCallback;
 import com.google.android.gms.nearby.connection.ConnectionOptions;
 import com.google.android.gms.nearby.connection.ConnectionResolution;
-import expo.modules.kotlin.activityresult.AppContextActivityResultLauncher
 import com.google.android.gms.nearby.connection.ConnectionType;
 import com.google.android.gms.nearby.connection.DiscoveredEndpointInfo;
 import com.google.android.gms.nearby.connection.DiscoveryOptions;
@@ -21,6 +21,7 @@ import com.google.android.gms.nearby.connection.PayloadCallback;
 import com.google.android.gms.nearby.connection.PayloadTransferUpdate;
 import com.google.android.gms.nearby.connection.Strategy;
 import com.google.android.gms.nearby.connection.ConnectionsClient;
+import com.google.android.gms.nearby.connection.ConnectionsStatusCodes
 import expo.modules.kotlin.AppContext
 import expo.modules.kotlin.exception.Exceptions
 import java.nio.charset.StandardCharsets.UTF_8
@@ -32,21 +33,18 @@ class HelphubNearbyModule : Module() {
   override fun definition() = ModuleDefinition {
     Name("HelphubNearby")
 
-    Function("getTheme") {
-      return@Function "system"
-    }
-
-    class ClientMessage() {
-
-    }
-
     val STRATEGY = Strategy.P2P_CLUSTER
+
+    val TAG = "Helphub-Nearby"
 
     lateinit var connectionsClient : ConnectionsClient
 
+    val SERVICE_ID = "helphub"
     val REQUEST_CODE_REQUIRED_PERMISSIONS = 1
 
     val messages : HashMap<String, String> = HashMap()
+    val connectedEndpoints : MutableList<String> = mutableListOf<String>()
+    val discoveredEndpoints : MutableList<String> = mutableListOf<String>()
 
     OnCreate {
       connectionsClient = Nearby.getConnectionsClient(context);
@@ -61,7 +59,11 @@ class HelphubNearbyModule : Module() {
     val payloadCallback : PayloadCallback = object : PayloadCallback() {
       override fun onPayloadReceived(endpointId: String, payload: Payload) {
         payload.asBytes()?.let {
-          messages[endpointId] = String(it, UTF_8)
+          if (messages.containsKey(endpointId)) {
+            messages[endpointId] = String(it, UTF_8)
+          } else {
+            messages.put(endpointId, String(it, UTF_8))
+          }
         }
       }
 
@@ -70,18 +72,114 @@ class HelphubNearbyModule : Module() {
     }
 
     val connectionLifecycleCallback : ConnectionLifecycleCallback = object : ConnectionLifecycleCallback() {
-      override fun onConnectionInitiated(p0: String, p1: ConnectionInfo) {
+      override fun onConnectionInitiated(endpointId: String, info: ConnectionInfo) {
+        connectionsClient.acceptConnection(endpointId, payloadCallback)
       }
 
-      override fun onConnectionResult(p0: String, p1: ConnectionResolution) {
+      override fun onConnectionResult(endpointId: String, resolution: ConnectionResolution) {
+        when (resolution.status.statusCode) {
+          ConnectionsStatusCodes.STATUS_OK -> {
+            Log.d(TAG, "Succesfully connected to endpoint $endpointId")
+            connectedEndpoints.add(endpointId)
+            discoveredEndpoints.remove(endpointId)
+            connectionsClient.stopAdvertising()
+            connectionsClient.stopDiscovery()
+          }
 
+          ConnectionsStatusCodes.STATUS_CONNECTION_REJECTED -> {
+            Log.d(TAG, "This or endpoint $endpointId rejected the connection.")
+          }
+
+          ConnectionsStatusCodes.STATUS_ERROR -> {
+            Log.w(TAG, "Connection to endpoint $endpointId failed with error ${resolution.status.statusMessage}")
+          }
+        }
       }
 
-      override fun onDisconnected(p0: String) {
-
+      override fun onDisconnected(endpointId: String) {
+        connectedEndpoints.remove(endpointId);
+        messages.remove(endpointId);
       }
 
     }
 
+    val endpointDiscoveryCallback = object : EndpointDiscoveryCallback() {
+      override fun onEndpointFound(endpointId: String, info: DiscoveredEndpointInfo) {
+        discoveredEndpoints.add(endpointId)
+      }
+
+      override fun onEndpointLost(endpointId: String) {
+        discoveredEndpoints.remove(endpointId)
+      }
+    }
+
+    fun requestConnection(endpointId : String) {
+      connectionsClient.requestConnection("Device", endpointId, connectionLifecycleCallback)
+        .addOnSuccessListener {
+          Log.d(TAG, "Requested connection to endpoint $endpointId.")
+        } .addOnFailureListener {
+          Log.w(TAG, "Connection request to endpoint $endpointId failed with: $it")
+        }
+    }
+
+    fun startAdvertising(name : String) {
+      val advertisingOptions : AdvertisingOptions = AdvertisingOptions.Builder().setStrategy(STRATEGY).build()
+      connectionsClient.startAdvertising(name, SERVICE_ID, connectionLifecycleCallback, advertisingOptions)
+        .addOnSuccessListener { Log.d(TAG, "Advertisement started successfully.")}
+        .addOnFailureListener {
+          Log.w(TAG, "Advertisement failed with $it")
+        }
+    }
+
+    fun startDiscovery(name : String) {
+      val discoveryOptions : DiscoveryOptions = DiscoveryOptions.Builder().setStrategy(STRATEGY).build()
+      connectionsClient.startDiscovery(name, endpointDiscoveryCallback, discoveryOptions)
+        .addOnSuccessListener {
+          Log.d(TAG, "Discovery started successfully.")
+        }.addOnFailureListener {
+          Log.w(TAG, "Discovery failed with $it")
+        }
+    }
+
+    Function("sendPayload") {
+        endpoint : String, payload : String -> sendPayload(endpoint, payload);
+    }
+
+    Function("startAdvertising") {
+      name : String -> startAdvertising(name)
+    }
+
+    Function("stopAdvertising") {
+      connectionsClient.stopAdvertising()
+    }
+
+    Function("startDiscovery") {
+      name : String -> startDiscovery(name)
+    }
+
+    Function("stopDiscovery") {
+      connectionsClient.stopDiscovery()
+    }
+
+    Function("requestConnection") {
+      endpoint : String -> requestConnection(endpoint)
+    }
+
+    Function("getMessages") {
+      return@Function messages
+    }
+
+    Function("getEndpointMessage") {
+      endpointId : String -> return@Function messages[endpointId]
+    }
+
+    Function("getDiscoveredEndpoints") {
+      return@Function discoveredEndpoints
+    }
+
+    Function("getConnectedEndpoints") {
+      return@Function connectedEndpoints
+    }
   }
 }
+
